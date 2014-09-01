e1511b42fb11981003a0c25417756d293179d7c7
bamiller2112 <brett.a.miller@gmail.com>
2014-08-31 23:56:55 -0400

    Changed project root directory from '/app' to '/'

---
+++ web.js
@@ -0,0 +1,22 @@
+/**
+ * Created by bamiller on 8/31/2014.
+ */
+
+var express = require('express');
+var app = express();
+var fs = require('fs');
+
+var buffer = fs.readFileSync('index.html','utf8');
+
+
+app.get('/', function(request, response) {
+    response.send(buffer);
+});
+
+var port = process.env.PORT ||8080; // disabled on dev server
+//var port = 8080; // enabled on dev server for browser feedback
+
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
+
